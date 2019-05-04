22fa6f82f02aeeb286e3dd2db9394bf14e76d81e
repenno <rapenno@gmail.com>
2019-05-04 09:32:04 -0700

    Remove unused JWS structures

--- jws/interface.go
+++ jws/interface.go
@@ -1,49 +1,5 @@
 package jws
 
-import (
-	"github.com/repenno/jwx-opa/jwa"
-)
-
-type EncodedSignature struct {
-	Protected string  `json:"protected,omitempty"`
-	Headers   Headers `json:"header,omitempty"`
-	Signature string  `json:"signature,omitempty"`
-}
-
-type EncodedSignatureUnmarshalProxy struct {
-	Protected string           `json:"protected,omitempty"`
-	Headers   *StandardHeaders `json:"header,omitempty"`
-	Signature string           `json:"signature,omitempty"`
-}
-
-type EncodedMessage struct {
-	Payload    string              `json:"payload"`
-	Signatures []*EncodedSignature `json:"signatures,omitempty"`
-}
-
-type EncodedMessageUnmarshalProxy struct {
-	Payload    string                            `json:"payload"`
-	Signatures []*EncodedSignatureUnmarshalProxy `json:"signatures,omitempty"`
-}
-
-type FullEncodedMessage struct {
-	*EncodedSignature // embedded to pick up flattened JSON message
-	*EncodedMessage
-}
-
-type FullEncodedMessageUnmarshalProxy struct {
-	*EncodedSignatureUnmarshalProxy // embedded to pick up flattened JSON message
-	*EncodedMessageUnmarshalProxy
-}
-
-// PayloadSigner generates Signature for the given Payload
-type PayloadSigner interface {
-	Sign([]byte) ([]byte, error)
-	Algorithm() jwa.SignatureAlgorithm
-	ProtectedHeader() Headers
-	PublicHeader() Headers
-}
-
 // Message represents a full JWS encoded message. Flattened serialization
 // is not supported as a struct, but rather it's represented as a
 // Message struct with only one `Signature` element.
